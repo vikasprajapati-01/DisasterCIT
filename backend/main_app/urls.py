415a25daa297542b9a1af13cde5b2683e28e1674
Mohie <mohiealdinsinan@gmail.com>
2025-03-06 16:26:49 +0530

    added ml model

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -4,10 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('users/', include('users.urls')),
     path('api/', include('predictions.urls')),
-=======
     path('', include('users.urls')),
->>>>>>> 628a513177b58ea56f6fdda0c700d5b75940cd09
 ]
