5ea16352540a5969e0a1b75f2dbe83f5567a1ea7
vishalpraja777 <vikasprajapati9980@gmail.com>
2025-03-06 14:42:39 +0530

    adding chatbot

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ChatbotLauncher from "./component/ChatbotLauncher";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+
+        <ChatbotLauncher />
       </body>
     </html>
   );
